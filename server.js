050447b5596bb3ee14be3902e6a86fb4cc9ae1a6
JadeJOy <jadehandsome30@gmail.com>
2021-02-15 10:19:13 -0800

    ggg 4

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "orbit-app/build")));
 
+// ---------------- ADD THIS ----------------
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/orbit-app/build/index.html"));
+});
+// --------------------------------
+
 app.post("/api/authenticate", async (req, res) => {
   try {
     const { email, password } = req.body;
